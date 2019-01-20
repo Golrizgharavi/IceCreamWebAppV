e3c1a3892bb6fd7033e2702fc7231331f0ee95ea
Golriz <golriz.gharavi@icloud.com>
2019-01-20 18:29:26 -0500

    Home page done no bug
    Complete Menu
    Add Item
    ;)

---
+++ IceCream Web Application v3/IceCream Web Application/App/controllers/menuController.js
@@ -0,0 +1,15 @@
+﻿
+
+app.controller('menuController', function (shopService) {
+
+    var promiseGet = shopService.getItems(); 
+
+    promiseGet.then(function (p1) {
+
+        vm.Items = p1.data
+    },
+        function (errorP1) {
+            alert('FAILURE LOADING Employees', errorP1)
+        });
+
+})
