68a99ff5e76af4f5e73a455fec8c33a76c949daa
Golriz <golriz.gharavi@icloud.com>
2019-01-21 22:16:03 -0500

    almost completed.
    just need to add comment control
    :)

---
+++ IceCream Web Application V4/IceCream Web Application/App/services/shopService.js
@@ -0,0 +1,45 @@
+﻿
+
+app.service('shopService', function ($http) {
+
+    this.SubmitEmailSubscribe = function (email) {
+
+       // alert(email);
+        return $http({
+
+            method: "POST",
+            url: "/GetData.aspx?q=1&e=" + email ,
+            headers: { 'Content-Type': 'application/json' },
+
+        }).success(function (data) {
+
+            //alert('success'+ JSON.stringify(data));
+
+        }).error(function (data) {
+
+            alert('Failed' + JSON.stringify(data));
+
+
+        })
+
+    }
+
+
+    this.getItems = function () {
+
+
+        return $http({
+            method: "GET",
+            url: "/Json/Items.json",
+            //headers: { 'Content-Type': 'application/json' },
+        }).success(function (data) {
+           // alert('success' + JSON.stringify(data))
+
+        }).error(function (data) {
+
+            alert(data);
+        });
+    }
+
+ 
+})
