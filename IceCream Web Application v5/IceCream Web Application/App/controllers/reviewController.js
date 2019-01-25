8aee8fe1d5231d71039a836c54c0f9c767cd5885
Golriz <golriz.gharavi@icloud.com>
2019-01-25 12:59:43 -0500

    Working on review section--submit bug fixed

---
+++ IceCream Web Application v5/IceCream Web Application/App/controllers/reviewController.js
@@ -0,0 +1,50 @@
+﻿
+
+app.controller('reviewController', function (shopService) {
+
+    var vm = this;
+    var promiseGet = shopService.getItems(); 
+    promiseGet.then(function (p1) {
+
+        vm.myReviews = p1.data.reviews;
+
+      
+    },
+        function (errorP1) {
+            alert('FAILURE LOADING Employees', errorP1)
+        });
+
+
+    vm.SubmitReview = function () {
+
+        var review = {
+
+            FName: vm.FirstName,
+            LName: vm.LastName
+        }
+       // alert(JSON.stringify(review) + 'Controller')
+        var GetResult= shopService.SubmitTheReview(review);     
+        GetResult.then(function (p1) {
+
+            vm.success = p1.data;
+            //alert(JSON.stringify(vm.success))
+            if (vm.success.s == 1) {
+                alert('Thanks for Submitting your Review ' + review.FName + ':)');
+                //vm.resetForm();
+            }
+            else {
+                alert("Error has occured while adding the employee" + JSON.stringify(data));
+            }
+
+        },
+            function (errorP1) {
+                alert('FAILURE LOADING Employees', errorP1)
+            });
+
+    }
+
+
+})
+
+
+    
